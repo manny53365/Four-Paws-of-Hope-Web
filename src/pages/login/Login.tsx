76f174fb69e1477273334f27c2af4cceb3be076a
Manny Rodriguez <mannyrodriguez011@gmail.com>
2025-12-11 00:25:34 -0500

    starter code for login screen

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,6 +3,9 @@ import { useLogin } from "../../hooks/useLogin";
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LoginIcon from '@mui/icons-material/Login';
 
 function Login() {
 
@@ -18,10 +21,20 @@ function Login() {
   return (
     <form className='auth-form' onSubmit={handleSubmit}>
             <h2>Log In</h2>
-            <TextField className='signInBtn' required id="outlined-required" label="Email" type='email' value={email} onChange={e => setEmail(e?.target.value)} />
-            <TextField className='signInBtn' required id="outlined-required" label="Password" type='password' value={password} onChange={e => setPassword(e?.target.value)} />
-            {!isPending && <Button variant="contained">Sign In</Button>}
-            {isPending && <Button variant="contained" disabled >Loading</Button>}
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid size={12}>
+                        <TextField className='signUp' required id="outlined-required" label="Email" type='email' value={email} onChange={e => setEmail(e?.target.value)} />
+                    </Grid>
+                    <Grid size={12}>
+                        <TextField className='signUp' required id="outlined-required" label="Password" type='password' value={password} onChange={e => setPassword(e?.target.value)} />
+                    </Grid>
+                    <Grid size={12}>
+                        {!isPending && <Button className='signUpBtn' startIcon={<LoginIcon />} variant="contained">Log In</Button>}
+                        {isPending && <Button className='signUpBtn' loading={isPending} variant="outlined" disabled>Logging in...</Button>}
+                    </Grid>
+                </Grid>
+            </Box>
             {error && <div className='error'>{error}</div>}
         </form>
   )
